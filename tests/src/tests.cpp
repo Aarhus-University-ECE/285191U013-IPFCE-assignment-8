19e860056978c25410f420a157386860ebc54829
Daniella1 <dani-ella95@live.com>
2021-10-15 11:21:09 +0200

    updated insertion_sort tests

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <assert.h>
 
 extern "C"
 {
@@ -10,55 +11,69 @@ extern "C"
 
 // See Catch2's documentation: https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md#scaling-up
 
+// For testing purposes only
+linked_list* convertArrayToLinkedList(int array[], int arraySize)
+{
+    linked_list* llPtr = createLinkedList();
+    for (int i = 0; i < arraySize; ++i)
+    {
+        insertFront(createNode(array[arraySize-i-1]), llPtr);
+    }
+    return llPtr;
+}
+// For testing purposes only
+void convertLinkedListToArray(linked_list* llPtr, int arr[])
+{
+    // Create array from linked list
+    node* curNode = llPtr->head;
+    int i = 0;
+    while(curNode != NULL)
+    {
+        arr[i++] = curNode->data;
+        curNode = curNode->next;
+    }
+}
+// For testing purposes only
+bool compareArrays(int arr1[], int arr2[], int arrSize)
+{
+    for (int i = 0; i < arrSize; ++i)
+    {
+        if (arr1[i] != arr2[i])
+            return false;
+    }
+    return true;
+}
+// For testing purposes only
+void testLinkedListInsertionSort(int randomArray[], int sortedArray[], int arraySize)
+{
+    linked_list * llPtr = convertArrayToLinkedList(randomArray, arraySize);
+    int linkedListArray[arraySize];
+    convertLinkedListToArray(llPtr, linkedListArray);
+    assert(compareArrays(randomArray, linkedListArray, arraySize) == true); // checks array and linked list are equal
+    sort(llPtr);
+    convertLinkedListToArray(llPtr, linkedListArray);
+    REQUIRE(compareArrays(sortedArray, linkedListArray, arraySize) == true); // check sorted array and sorted linked list are equal
+
+}
+
 TEST_CASE("insertion_sort")
 {
-    // Test full list with both negative and positive numbers
-    linked_list* llPtr = createLinkedList();
-    for(int i = 1; i < 4; i++)
-    {
-        insertFront(createNode(i), llPtr);
-    }
-    insertFront(createNode(33), llPtr);
-    insertFront(createNode(11), llPtr);
-    insertFront(createNode(22), llPtr);
-    insertFront(createNode(-3), llPtr);
-
+    // Test full random list
     int testerList[7] = {-3,22,11,33,3,2,1};
-    REQUIRE(testerList[0] == llPtr->head->data);
-    node* curNode = llPtr->head;
-    for (int i = 1; i < 7; ++i)
-    {
-        REQUIRE(testerList[i] == curNode->next->data);
-        curNode = curNode->next;
-    }
-    
-    sort(llPtr);
-    
     int sortedList[7] = {-3,1,2,3,11,22,33};
-    REQUIRE(sortedList[0] == llPtr->head->data);
-    curNode = llPtr->head;
-    for (int i = 1; i < 7; ++i)
-    {
-        REQUIRE(sortedList[i] == curNode->next->data);
-        curNode = curNode->next;
-    }
+    testLinkedListInsertionSort(testerList, sortedList, 7);
 
-    // Test list of size 3
-    linked_list* llPtr2 = createLinkedList();
-    insertFront(createNode(1), llPtr2);
-    insertFront(createNode(-1), llPtr2);
-    insertFront(createNode(0), llPtr2);
+
+    // Test list with both negative and positive numbers
     int testerList2[3] = {0,-1,1};
-    curNode = llPtr2->head;
-    REQUIRE(testerList2[0] == curNode->data);
-    REQUIRE(testerList2[1] == curNode->next->data);
-    REQUIRE(testerList2[2] == curNode->next->next->data);
-    sort(llPtr2);
-    curNode = llPtr2->head;
-    REQUIRE(-1 == curNode->data);
-    REQUIRE(0 == curNode->next->data);
-    REQUIRE(1 == curNode->next->next->data);
+    int sortedList2[3] = {-1,0,1};
+    testLinkedListInsertionSort(testerList2, sortedList2, 3);
 
+
+    // Test list with duplicate values
+    int testerList3[10] = {6,6,5,4,2,3,3,3,2,1};
+    int sortedList3[10] = {1,2,2,3,3,3,4,5,6,6};
+    testLinkedListInsertionSort(testerList3, sortedList3, 10);
 }
 
 
