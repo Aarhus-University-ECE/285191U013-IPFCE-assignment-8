c3594fc170353b9d4a947347e31eefb9e2569da7
kpbaks <kristoffer.pbs@tuta.io>
2023-10-15 14:05:03 +0200

    feat: finish tests for linked list

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,10 @@
 #include "catch2/matchers/catch_matchers.hpp"
+#include <assert.h>
 
 extern "C" {
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "insertion_sort.h"
 #include "queue.h"
 }
@@ -140,116 +144,135 @@ TEST_CASE("queue", "[queue]") {
     REQUIRE(xs[4] == ys[4]);
 }
 
+node *ll_create_node(int x) {
+  node *n = (node *)malloc(sizeof(node));
+  if (!n) {
+    fprintf(stderr, "%s:%d Allocation with malloc() failed.\n", __FILE__,
+            __LINE__);
+    exit(1);
+  }
 
+  n->data = x;
+  n->next = NULL;
 
-// For testing purposes only
-linked_list* convertArrayToLinkedList(int array[], int arraySize)
-{
-    linked_list* llPtr = createLinkedList();
-    for (int i = 0; i < arraySize; ++i)
-    {
-        insertFront(createNode(array[arraySize-i-1]), llPtr);
-    }
-    return llPtr;
+  return n;
 }
-// For testing purposes only
-void convertLinkedListToArray(linked_list* llPtr, int arr[])
-{
-    // Create array from linked list
-    node* curNode = llPtr->head;
+
+void ll_append(node *head, int x) {
+    assert(head != NULL);
+    node* p = head;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+
+    node* n = ll_create_node(x);
+    p->next = n;
+}
+
+node* ll_from_array(const int *array, const int len) {
+    node* list = (node *)malloc(sizeof(node));
+    if (!list) {
+        fprintf(stderr, "%s:%d Allocation with malloc() failed.\n", __FILE__,
+                __LINE__);
+        exit(1);
+    }
+    list->next = NULL;
+
+    for (int i = 0; i < len; i++) {
+        ll_append(list, array[i]);
+    }
+
+    return list;
+}
+
+int ll_size(node *head) {
+    assert(head != NULL);
+    
+    int size = 0;
+    while (head != NULL) {
+        head = head->next;
+        size++;
+    }
+
+    return size - 1; // The first node is the head (a sentinel)
+}
+
+int* ll_to_array(node *head) {
+    assert(head != NULL);
+    const int len = ll_size(head);
+    int *arr = (int *)malloc(sizeof(int) * len);
+    if (!arr) {
+        fprintf(stderr, "%s:%d Allocation with malloc() failed.\n", __FILE__,
+                __LINE__);
+        exit(1);
+    }
+
     int i = 0;
-    while(curNode != NULL)
-    {
-        arr[i++] = curNode->data;
-        curNode = curNode->next;
+    node* p = head->next; // The first node is the head (a sentinel)
+    while (p != NULL) {
+        arr[i] = p->data;
+        i++;
+        p = p->next;
     }
+    
+    return arr;
 }
-// For testing purposes only
-bool compareArrays(int arr1[], int arr2[], int arrSize)
-{
-    for (int i = 0; i < arrSize; ++i)
-    {
-        if (arr1[i] != arr2[i])
+
+bool arrays_equal(const int *arr1, const int *arr2, const int len) {
+    assert(arr1 != NULL);
+    assert(arr2 != NULL);
+    assert(len > 0);
+    
+    for (int i = 0; i < len; i++) {
+        if (arr1[i] != arr2[i]) {
             return false;
+        }
     }
     return true;
 }
-// For testing purposes only
-void testLinkedListInsertionSort(int randomArray[], int sortedArray[], int linkedListArray[], int arraySize)
-{
-    linked_list * llPtr = convertArrayToLinkedList(randomArray, arraySize);
-    convertLinkedListToArray(llPtr, linkedListArray);
-    assert(compareArrays(randomArray, linkedListArray, arraySize) == true); // checks array and linked list are equal
-    sort(llPtr);
-    convertLinkedListToArray(llPtr, linkedListArray);
-    REQUIRE(compareArrays(sortedArray, linkedListArray, arraySize) == true); // check sorted array and sorted linked list are equal
 
+
+TEST_CASE("positive-numbers", "[insertion_sort]") {
+    const int input[] =        {5, 22, 11, 33, 3, 2, 1};
+    const int len = sizeof(input) / sizeof(int);
+    const int input_sorted[len] = {1, 2, 3, 5, 11, 22, 33};
+
+    node* list = ll_from_array(input, len);
+    isort(list);
+    int* arr = ll_to_array(list);
+    REQUIRE(arrays_equal(arr, input_sorted, len));
 }
 
+TEST_CASE("negative-numbers", "[insertion_sort]") {
+    const int input[] =        {-5, -22, -11, -33, -3, -2, -1};
+    const int len = sizeof(input) / sizeof(int);
+    const int input_sorted[len] = {-33, -22, -11, -5, -3, -2, -1};
 
-
-TEST_CASE("insertion_sort")
-{
-    // Test full random list
-    int testerList[7] = {-3,22,11,33,3,2,1};
-    int sortedList[7] = {-3,1,2,3,11,22,33};
-    int linkedListArray[7];
-    testLinkedListInsertionSort(testerList, sortedList, linkedListArray, 7);
-
-
-    // Test list with both negative and positive numbers
-    int testerList2[3] = {0,-1,1};
-    int sortedList2[3] = {-1,0,1};
-    int linkedListArray2[3];
-    testLinkedListInsertionSort(testerList2, sortedList2, linkedListArray2, 3);
-
-
-    // Test list with duplicate values
-    int testerList3[10] = {6,6,5,4,2,3,3,3,2,1};
-    int sortedList3[10] = {1,2,2,3,3,3,4,5,6,6};
-    int linkedListArray3[10];
-    testLinkedListInsertionSort(testerList3, sortedList3, linkedListArray3, 10);
+    node* list = ll_from_array(input, len);
+    isort(list);
+    int* arr = ll_to_array(list);
+    REQUIRE(arrays_equal(arr, input_sorted, len));
 }
 
+TEST_CASE("2-numbers", "[insertion_sort]") {
+    const int input[] =        {1, 10};
+    const int len = sizeof(input) / sizeof(int);
+    const int input_sorted[len] = {1, 10};
 
-TEST_CASE("linked_queue")
-{
-    // Test inserting random values into queue and check size
-    int valuesToInsert[4] = {-5,10,0,5};
-    queue qq;
-    init_queue(&qq);
-    int queueSize = 0;
-    REQUIRE(empty(&qq) == 1);
-    for (int i = 0; i < 4; ++i)
-    {
-        enqueue(&qq, valuesToInsert[i]);
-        REQUIRE(qq.size == ++queueSize);
-    }
+    node* list = ll_from_array(input, len);
+    isort(list);
+    int* arr = ll_to_array(list);
+    REQUIRE(arrays_equal(arr, input_sorted, len));
+}
 
-    // Test empty function with empty queue
-    queue q;
-    init_queue(&q);
-    REQUIRE(empty(&q) == 1);
+TEST_CASE("numbers", "[insertion_sort]") {
+    const int input[] = { 3, 5, 11, 2, 16, 18, 6, 4, 14, 9, 7, 20, 10, 19, 8, 1, 17, 13, 15, 12 };
+    const int len = sizeof(input) / sizeof(int);
+    const int input_sorted[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
-    // Test empty function with non-empty queue
-    queue q1;
-    init_queue(&q1);
-    enqueue(&q1, 5);
-    REQUIRE(empty(&q1) == 0);
-    dequeue(&q1);
-    REQUIRE(empty(&q1) == 1); 
-
-    // Test enqueue then dequeue
-    enqueue(&q1,-5);
-    enqueue(&q1,10);
-    enqueue(&q1,0);
-    enqueue(&q1,5);
-   
-
-    REQUIRE(-5 == dequeue(&q1));
-    REQUIRE(10 == dequeue(&q1));
-    REQUIRE(0 == dequeue(&q1));
-    REQUIRE(5 == dequeue(&q1));
-
+    node* list = ll_from_array(input, len);
+    isort(list);
+    int* arr = ll_to_array(list);
+    REQUIRE(arrays_equal(arr, input_sorted, len));
 }
 
